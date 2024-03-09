bde8112ee373ffee7b8033ce81baa1152130f8b2
Ilay-Benjamin <ilaypro1222@gmail.com>
2024-03-09 16:08:53 +0200

    SNAPSHOT 10.1

--- dni.js
+++ dni.js
@@ -1,9 +1,9 @@
 
 //import 'https://code.jquery.com/jquery-3.3.1.min.js';
 
-//import {Dni} from 'https://ilay-apis.online/Libraries/Library-7/dni-crm/dni.js';
+import {Dni} from 'https://ilay-apis.online/Libraries/Library-7/dni-crm/dni.js';
 
-import {Dni} from './../crm_library/dni-crm/dni.js';
+//import {Dni} from './../crm_library/dni-crm/dni.js';
 
 const dni = new Dni();
 
